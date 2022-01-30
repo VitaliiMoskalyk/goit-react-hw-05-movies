8574ff35de46f2356bd72c49c31149d43a2d5584
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-01-30 17:12:19 +0200

    del_nothing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route,Navigate } from "react-router-dom";
 
 const HomePage = lazy(() => import('./views/homePage'));
 const MoviesPage = lazy(() => import('./views/movies'));
-const Nothing = lazy(() => import('./views/notFound/NothingHere'));
 const FilmOverview = lazy(() => import('./views/filmOverview'));
 const Layout = lazy(() => import('./components/layout/Layout'));
 const Cast = lazy(() => import('./components/cast'));
